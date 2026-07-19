1821983f911bba0b18430673fd59e5875f6bc17e
Atambek7 <atambek.tiger12@gmail.com>
2026-07-19 21:04:09 +0500

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { z } from "zod";
+
+const server = new McpServer({
+    name: "book-mcp",
+    version: "1.0.0",
+});
+
+server.registerTool(
+    "hello",
+    {
+        title: "Hello",
+        description: "Проверка работы MCP",
+        inputSchema: {
+            name: z.string().optional(),
+        },
+    },
+    async ({ name }) => {
+        return {
+            content: [
+                {
+                    type: "text",
+                    text: `Привет ${name ?? "мир"}!`,
+                },
+            ],
+        };
+    }
+);
+
+const transport = new StdioServerTransport();
+
+await server.connect(transport);
